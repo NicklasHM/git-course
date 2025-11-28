7dac982c0a00ab27fbf7733a7e913ace317b5756
Nick <153918374+nicklas-mikkelsen@users.noreply.github.com>
2025-11-28 10:21:14 +0100

    Update merge-conflicts.js

--- merge-conflicts.js
+++ merge-conflicts.js
@@ -5,10 +5,14 @@ const add = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
 
 const divide = (n1, n2) => {
+    if (n2 === 0) {
+        throw error("cannot divide by 0");
+    }
     return n1 / n2;
 }
 
 export {
     add,
-    subtract
+    subtract,
+    divde
 }
